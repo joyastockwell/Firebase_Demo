eec72b7fec6d9e13d29e99e1bd01296ea5ae6ad2
joyastockwell <stockwja@rose-hulman.edu>
2021-06-21 12:20:17 -0700

    One image appears
    Can get cow image to appear at top of page. Working to get each item's image to appear

--- app.js
+++ app.js
@@ -4,20 +4,45 @@ const form = document.querySelector('#add-item-form');
 var storage = firebase.storage();
 // Create a storage reference from our storage service
 var storageRef = storage.ref()
-/*var imgRef = storageRef.child('Watermelon.jpg');
-console.log(imgRef);*/
+
+function getFromStorage(stringName) {
+    imgRef = storageRef.child(stringName.concat(".png"));
+    /*imgRef.getDownloadURL().then(function(url) {
+  var pulledProfileImage = url;
+  dataArray.push(pulledProfileImage);
+});*/
+    firebase.auth().signInAnonymously().then(function() {
+        imgRef.getDownloadURL().then(function(url) {
+            console.log("hi, daniel!");
+            var pulledImage = url;
+            return pulledImage;
+            //return url
+        }) /*                          {
+            // Once we have the download URL, we set it to our img element
+            document.querySelector('img').src = url;
+            console.log("please print :D");
+        }).catch(function(error) {
+            // If anything goes wrong while getting the download URL, log the error
+            console.error(error);
+            console.log("hullo hullo");
+        });*/
+    });
+}
 
 function renderCafe(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let color = document.createElement('span');
+    let img = document.createElement('img');
 
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     color.textContent = doc.data().color;
+    img.src = getFromStorage(name.textContent.replace(" ", "_"));
 
     li.appendChild(name);
     li.appendChild(color);
+    li.appendChild(img);
 
     itemList.appendChild(li);
 }
@@ -28,7 +53,7 @@ db.collection('Items').get().then(snapshot => {
     })
 })
 
-form.addEventListener('submit', (e) => {
+/*form.addEventListener('submit', (e) => {
     e.preventDefault();
     db.collection('Items').add({
         name:form.name.value,
@@ -36,19 +61,13 @@ form.addEventListener('submit', (e) => {
     });
     form.name.value = '';
     form.city.value = '';
-})
+})*/
 
-
-/*firebase.auth().signInAnonymously().then(function() {
-    console.log("hi, adam!");
-    imgRef.getDownloadURL().then(function(url)                           {
-        // Once we have the download URL, we set it to our img element
+imgRef2 = storageRef.child("cowimage.jpeg");
+firebase.auth().signInAnonymously().then(function() {
+    imgRef2.getDownloadURL().then(function(url) {
+        console.log("hi, daniel again!");
         document.querySelector('img').src = url;
-        //document.getElementsByClassName("coffee_shop_img");
-        console.log("please print :D");
-    }).catch(function(error) {
-        // If anything goes wrong while getting the download URL, log the error
-        console.error(error);
-        console.log("hullo hullo");
-    });
-});*/
+        //return url
+    })
+});
