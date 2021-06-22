1c41763299aaf1581c4d6454c0a25edda23ea98c
joyastockwell <stockwja@rose-hulman.edu>
2021-06-22 10:44:12 -0700

    Still working

--- app.js
+++ app.js
@@ -5,37 +5,6 @@ var storage = firebase.storage();
 // Create a storage reference from our storage service
 var storageRef = storage.ref();
 
-imgRef2 = storageRef.child("Gummy_Shark.png");
-firebase.auth().signInAnonymously().then(function() {
-    imgRef2.getDownloadURL().then(function(url) {
-        document.querySelector('img').src = url;
-        //return url
-    })
-});
-
-function getFromStorage(stringName) {
-    console.log(stringName.concat(".png"));
-    imgRef = storageRef.child(stringName.concat(".png"));
-    /*imgRef.getDownloadURL().then(function(url) {
-  var pulledProfileImage = url;
-  dataArray.push(pulledProfileImage);
-});*/
-    firebase.auth().signInAnonymously().then(function() {
-        imgRef.getDownloadURL().then(function(url) {
-            var pulledImage = url;
-            return pulledImage;
-            //return url
-        }) /*                          {
-            // Once we have the download URL, we set it to our img element
-            document.querySelector('img').src = url;
-            console.log("please print :D");
-        }).catch(function(error) {
-            // If anything goes wrong while getting the download URL, log the error
-            console.error(error);
-            console.log("hullo hullo");
-        });*/
-    });
-}
 
 function renderCafe(doc) {
     let li = document.createElement('li');
@@ -50,9 +19,9 @@ function renderCafe(doc) {
     currImgRef = storageRef.child(name.textContent.replace(" ", "_").concat(".png"));
     console.log(name.textContent.replace(" ", "_"));
     firebase.auth().signInAnonymously().then(function() {
+        console.log(currImgRef.name);
         currImgRef.getDownloadURL().then(function(url) {
             image.src = url;
-            //return url
         })
     });
   //  img.src = getFromStorage(name.textContent.replace(" ", "_"));
