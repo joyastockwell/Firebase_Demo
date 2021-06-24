28e2e7744006e59dc13346589670bc347f1a8e76
dannomo <85249367+dannomo@users.noreply.github.com>
2021-06-24 14:10:55 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -5,54 +5,27 @@ var storage = firebase.storage();
 // Create a storage reference from our storage service
 var storageRef = storage.ref();
 
-imgRef2 = storageRef.child("Gummy_Shark.png");
-firebase.auth().signInAnonymously().then(function() {
-    imgRef2.getDownloadURL().then(function(url) {
-        document.querySelector('img').src = url;
-        //return url
-    })
-});
-
-function getFromStorage(stringName) {
-    console.log(stringName.concat(".png"));
-    imgRef = storageRef.child(stringName.concat(".png"));
-    /*imgRef.getDownloadURL().then(function(url) {
-  var pulledProfileImage = url;
-  dataArray.push(pulledProfileImage);
-});*/
-    firebase.auth().signInAnonymously().then(function() {
-        imgRef.getDownloadURL().then(function(url) {
-            var pulledImage = url;
-            return pulledImage;
-            //return url
-        }) /*                          {
-            // Once we have the download URL, we set it to our img element
-            document.querySelector('img').src = url;
-            console.log("please print :D");
-        }).catch(function(error) {
-            // If anything goes wrong while getting the download URL, log the error
-            console.error(error);
-            console.log("hullo hullo");
-        });*/
-    });
-}
 
 function renderCafe(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let color = document.createElement('span');
     let image = document.createElement('img');
+    let imageName = document.createElement('span');
 
     li.setAttribute('data-id', doc.id);
+    imageName.textContent = doc.data().imageName;
     name.textContent = doc.data().name;
     color.textContent = doc.data().color;
+    var currImgRef = storageRef.child(doc.id.concat("/").concat(((imageName.textContent))));
+    //var currImgRef = storageRef.child(doc.id.concat("/").concat((name.textContent.replace(" ", "_").concat(".png"))));
 
-    currImgRef = storageRef.child(name.textContent.replace(" ", "_").concat(".png"));
+    console.log("currImgRef is " + currImgRef);
     console.log(name.textContent.replace(" ", "_"));
     firebase.auth().signInAnonymously().then(function() {
+        console.log(currImgRef.name);
         currImgRef.getDownloadURL().then(function(url) {
             image.src = url;
-            //return url
         })
     });
   //  img.src = getFromStorage(name.textContent.replace(" ", "_"));
@@ -70,13 +43,14 @@ db.collection('Items').get().then(snapshot => {
     })
 })
 
-/*form.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     db.collection('Items').add({
         name:form.name.value,
-        city: form.city.value
+        color: form.color.value
     });
+    console.log("hullo");
     form.name.value = '';
-    form.city.value = '';
-})*/
+    form.color.value = '';
+})
 
